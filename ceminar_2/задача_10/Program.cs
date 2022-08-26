22eaed3c864fa3855677382a5e9427cbf0f02c3b
KEA <lena_konovalova@rambler.ru>
2022-08-26 13:45:22 +0300

    10 15

---
+++ ceminar_2/задача_10/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintSecondNum()
+{
+    int x = new Random().Next (100, 1000);
+    Console.WriteLine($"{x}");
+    int a = x%100;
+    int b = a/10;
+    Console.WriteLine($"второе число {b}");
+}
+
+PrintSecondNum();
---
+++ ceminar_2/задача_15/Program.cs
@@ -0,0 +1,23 @@
+﻿void PrintDenX()
+
+{
+Console.WriteLine("Ведите число от 1 до 7 ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1|| num == 2|| num == 3|| num == 4||num == 5 )
+{
+    Console.WriteLine("нет");
+}
+
+if (num == 6|| num == 7)
+{
+    Console.WriteLine("да");
+}
+
+if (num <1|| num >7)
+{
+    Console.WriteLine("В неделе только 7 дней");
+}
+}
+PrintDenX();
+
+
