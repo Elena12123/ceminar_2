aab4c59000333af4ac08cb9d667f3676f92da4ca
KEA <lena_konovalova@rambler.ru>
2022-08-29 13:56:24 +0300

    13 zadacha

---
+++ ceminar_2/задача_13/Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintNum3()
+{
+// Console.WriteLine("Введите число ");
+// string N = Console.ReadLine();
+// int x = N.Length;
+// Console.WriteLine(Length);
+
+Console.WriteLine("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+string N = Convert.ToString(x);
+
+
+if (N.Length > 2)
+{
+    Console.WriteLine ("Третья цифра в числе "+ N [2]);
+
+}
+
+else
+{
+    Console.WriteLine ($"Третьей цифры нет ");
+}
+
+}
+
+  
+PrintNum3();
